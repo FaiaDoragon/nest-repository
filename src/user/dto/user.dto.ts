e409a99b9d32010066a198ffd06c1a20febdfec6
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2024-03-02 15:22:36 -0500

    userdto creado y entity de usuarios creada

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,1 +1,13 @@
-export class UserDto {}
+export class UserDto {
+    id: number;
+    name: string;
+    lastName: string;
+    password: string;
+    email: string;
+    phoneNumber: string | null;
+    address: string | null;
+    city: string | null;
+    createdAt: Date | null;
+    updatedAt: Date | null;
+  }
+  
---
+++ src/user/entities/Usuarios.ts
@@ -0,0 +1,35 @@
+import { Column, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
+
+@Index("email", ["email"], { unique: true })
+@Entity("usuarios", { schema: "USERSDB" })
+export class Usuarios {
+  @PrimaryGeneratedColumn({ type: "int", name: "id" })
+  id: number;
+
+  @Column("varchar", { name: "name", length: 50 })
+  name: string;
+
+  @Column("varchar", { name: "last_name", length: 50 })
+  lastName: string;
+
+  @Column("varchar", { name: "password", length: 100 })
+  password: string;
+
+  @Column("varchar", { name: "email", unique: true, length: 100 })
+  email: string;
+
+  @Column("varchar", { name: "phone_number", nullable: true, length: 15 })
+  phoneNumber: string | null;
+
+  @Column("varchar", { name: "address", nullable: true, length: 100 })
+  address: string | null;
+
+  @Column("varchar", { name: "city", nullable: true, length: 50 })
+  city: string | null;
+
+  @Column("date", { name: "created_at", nullable: true })
+  createdAt: string | null;
+
+  @Column("date", { name: "updated_at", nullable: true })
+  updatedAt: string | null;
+}
